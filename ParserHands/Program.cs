ebd34e9a26fb36a118fed04650765b7bf7cc8d6e
Taras Didukh <tdidukh@iMac-Taras.local>
2018-09-04 08:59:53 +0300

    Import ready parser

---
+++ ParserHands/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using HandHistories.Objects.Hand;
+using HandHistories.Parser.Parsers.Base;
+using HandHistories.Parser.Parsers.Factory;
+
+namespace ParserHands
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            IHandHistoryParserFactory handHistoryParserFactory = new HandHistoryParserFactoryImpl();
+
+            // Get the correct parser from the factory.
+            IHandHistoryParser handHistoryParser = handHistoryParserFactory.GetFullHandHistoryParser(HandHistories.Objects.GameDescription.SiteName.PokerStars);
+
+            var  handText = System.IO.File.ReadAllText("input.txt"); 
+
+            try
+            {
+                // The true causes hand-parse errors to get thrown. If this is false, hand-errors will
+                // be silent and null will be returned.
+                HandHistory handHistory = handHistoryParser.ParseFullHandHistory(handText, true);
+
+            }
+            catch (Exception ex) // Catch hand-parsing exceptions
+            {
+                Console.WriteLine("Parsing Error: {0}", ex.Message); // Example logging.
+            }
+        }
+    }
+}
